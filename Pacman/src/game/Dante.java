7b6bb482ac3b0569106929963998a2e7e83dd122
cristian <crisdamencast@gmail.com>
2020-09-03 18:31:50 -0500

    add core.jar and create dante class

--- Pacman/src/game/Dante.java
+++ Pacman/src/game/Dante.java
@@ -1,6 +1,14 @@
 package game;
 
+import java.awt.Color;
+
+import processing.core.PApplet;
+import processing.core.PImage;
+
 public class Dante {
+	
+	PApplet app;
+	PImage nave;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
