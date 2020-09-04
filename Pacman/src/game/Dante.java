751b7c9ee3ecc3e874df7b85c14f3023bdb8efb3
cristian <crisdamencast@gmail.com>
2020-09-03 19:06:32 -0500

    movement Dante, image too

--- Pacman/src/game/Dante.java
+++ Pacman/src/game/Dante.java
@@ -6,13 +6,55 @@ import processing.core.PApplet;
 import processing.core.PImage;
 
 public class Dante {
-	
+
 	PApplet app;
-	PImage nave;
+	PImage dante;
+	int x;
+	int y;
+	int speed;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public Dante(int x, int y, int s, PApplet app) {
+		this.x = x;
+		this.y = y;
+		this.speed = s;
+		this.app=app;
+		dante= app.loadImage("./Image/dante.png");
 	}
 
+	protected void paint() {
+		app.image(dante,x,y);
+		//app.fill(255,0,0);
+		//app.rect(x, y, 50, 50);		
+	}
+
+	protected void moveLeft() {
+
+		x -=speed;
+		if(x <= 0){
+			x +=speed;
+		}
+	}
+	protected void moveRight() {
+
+		x +=speed;
+		if(x >= 375){
+			x -=speed;					
+		}
+
+	}
+	protected void moverUp() {
+
+		y +=speed;
+		if(y >= 0){
+			y -=speed;					
+		}
+	}
+	protected void moverDown() {
+
+		y +=speed;
+		if(y >= 667){
+			y -=speed;				
+		}
+	}
 }
+
